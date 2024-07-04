4c506dbd0999d848bae260aa01018708aaa267d3
YashPradhan2002 <yashpradhanksp@gmail.com>
2024-07-04 16:35:11 +0530

    w8 animations

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
  
   
-    // apiUrl: 'https://pixel-server-theta.vercel.app/', // API base URL
-    // uploadUrl: 'https://pixel-server-theta.vercel.app/' // Uploads folder URL
+    apiUrl: 'https://pixel-admin-server.vercel.app/', // API base URL
+    uploadUrl: 'https://pixel-admin-server.vercel.app/uploads/' // Uploads folder URL
 
-    apiUrl: 'http://localhost:5000/', // API base URL
-    uploadUrl: 'http://localhost:5000/' // Uploads folder URL
+    // apiUrl: 'http://localhost:5000/', // API base URL
+    // uploadUrl: 'http://localhost:5000/' // Uploads folder URL
   };
   
   export default config;
