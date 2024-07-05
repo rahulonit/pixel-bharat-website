3244213b38763a2fdc2dde10644ef62943014f97
YashPradhan2002 <yashpradhanksp@gmail.com>
2024-07-05 13:50:25 +0530

    render.co add

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 const config = {
  
-  
-    apiUrl: 'https://pixel-admin-server.vercel.app/', // API base URL
-    uploadUrl: 'https://pixel-admin-server.vercel.app/uploads/' // Uploads folder URL
+  apiUrl: 'https://pixel-admin-server.onrender.com/', // API base URL
+  uploadUrl: 'https://pixel-admin-server.onrender.com/uploads/' // Uploads folder URL
+    // apiUrl: 'https://pixel-admin-server.vercel.app/', // API base URL
+    // uploadUrl: 'https://pixel-admin-server.vercel.app/uploads/' // Uploads folder URL
 
     // apiUrl: 'http://localhost:5000/', // API base URL
     // uploadUrl: 'http://localhost:5000/' // Uploads folder URL
